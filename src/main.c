c2916a6fcd7f179112534e3f343330b19a4e546b
CCStudios <50129568+CodeCrashStudios@users.noreply.github.com>
2024-04-14 20:54:13 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,22 +5,22 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Img Edit");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);
 
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(WHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, BLACK);
 
         EndDrawing();
     }
 
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow();
 
     return 0;
 }
