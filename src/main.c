0a9a4a1d4cf4585b6db0a12ff44858bc28f9f067
CCStudios <50129568+CodeCrashStudios@users.noreply.github.com>
2024-04-14 23:57:59 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,26 +2,27 @@
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+  const int screenWidth = 800;
+  const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Img Edit");
+  SetConfigFlags(FLAG_VSYNC_HINT);
+  
+  InitWindow(screenWidth, screenHeight, "Img Edit");
 
-    SetTargetFPS(60);
+  //SetTargetFPS(60);
+  
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
+  while (!WindowShouldClose())
+  {
+    BeginDrawing();
 
-            ClearBackground(WHITE);
+    ClearBackground(WHITE);
+    DrawRectangle(0, 0, GetMouseX(), GetMouseY(), RED);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, BLACK);
+    EndDrawing();
+  }
 
-        EndDrawing();
-    }
+  CloseWindow();
 
-    CloseWindow();
-
-    return 0;
+  return 0;
 }
-
